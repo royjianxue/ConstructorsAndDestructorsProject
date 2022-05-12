da637eb695b03a0b9ebe7b9e5eac0956004463da
Jian Xue <royjianxue@yahoo.com>
2022-05-12 15:49:05 -0400

    Add project files.

---
+++ ConstructorsAndDestructors/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConstructorsAndDestructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] people = new Person[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("First name: ");
+                string input = Console.ReadLine();
+                people[i] = new Person(input);
+            }
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; } 
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Hello! My name is {Name}";
+        }
+
+        ~Person() // this will destroy the instance
+        {
+            Name = string.Empty;
+        }
+
+    }
+
+
+
+}
